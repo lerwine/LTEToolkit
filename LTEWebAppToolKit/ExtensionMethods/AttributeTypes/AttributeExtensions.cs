a19cc95e9270921baace983f0d71df89c3807eb5
Leonard <lenny@erwinefamily.net>
2014-10-29 10:26:19 -0400

    Created AttributeExtensions.cs

---
+++ LTEWebAppToolKit/ExtensionMethods/AttributeTypes/AttributeExtensions.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Erwine.Leonard.T.Toolkit.WebApp.ExtensionMethods.AttributeTypes
+{
+    public static class AttributeExtensions
+    {
+        public static TAttribute[] GetAttributesOfType<TAttribute>(this MemberInfo memberInfo, bool inherit)
+            where TAttribute : Attribute
+        {
+            if (memberInfo == null)
+                return new TAttribute[0];
+
+            return memberInfo.GetCustomAttributes(typeof(TAttribute), inherit).OfType<TAttribute>().ToArray();
+        }
+    }
+}
