bad92a4f00a84d40e4f9b32634ef87a936681001
Leonard <lenny@erwinefamily.net>
2014-10-29 18:23:23 -0400

    Update DistinctBucket.cs

--- SimplePaletteQuantizerModLTE/Quantizers/DistinctSelection/DistinctBucket.cs
+++ SimplePaletteQuantizerModLTE/Quantizers/DistinctSelection/DistinctBucket.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
-namespace SimplePaletteQuantizer.Quantizers.DistinctSelection
+namespace Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.Quantizers.DistinctSelection
 {
     public class DistinctBucket
     {
