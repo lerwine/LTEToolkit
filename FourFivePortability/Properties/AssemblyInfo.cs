330a9e0d3dc91a549995ac99eebb81ba6b144a37
Leonard <lenny@erwinefamily.net>
2014-10-29 16:05:14 -0400

    Create AssemblyInfo.cs

---
+++ FourFivePortability/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("FourFive Portability Project")]
+[assembly: AssemblyDescription("Provides interfaces and classes which exist in .NET 4.5 in order to assist with code portability from .NET 4.5 to .NET 4.0.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Leonard T. Erwine")]
+[assembly: AssemblyProduct("FourFivePortability")]
+[assembly: AssemblyCopyright("Leonard T. Erwine, October 2014, Code Project Open License (CPOL) 1.02")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ef10c782-3ee2-4f4c-877f-73cb7909f89a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
