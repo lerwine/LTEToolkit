1c87f035c2876279fda0df00a2f71f45a031aa1e
Leonard <lenny@erwinefamily.net>
2014-10-29 18:08:22 -0400

    Update IColorDitherer.cs

--- SimplePaletteQuantizerModLTE/Ditherers/IColorDitherer.cs
+++ SimplePaletteQuantizerModLTE/Ditherers/IColorDitherer.cs
@@ -1,9 +1,9 @@
 using System;
-using SimplePaletteQuantizer.Helpers;
-using SimplePaletteQuantizer.PathProviders;
-using SimplePaletteQuantizer.Quantizers;
+using Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.Helpers;
+using Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.PathProviders;
+using Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.Quantizers;
 
-namespace SimplePaletteQuantizer.Ditherers
+namespace Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.Ditherers
 {
     public interface IColorDitherer : IPathProvider
     {
