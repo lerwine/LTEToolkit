ffc6830fdf2e8a15e55d3bfe54ed03388723a7dd
Leonard <lenny@erwinefamily.net>
2014-10-29 10:25:14 -0400

    Created AppRoleExtensions.cs

---
+++ LTEWebAppToolKit/CustomConfigurationSection/ExtensionMethods/AppRoleExtensions.cs
@@ -0,0 +1,18 @@
+namespace Erwine.Leonard.T.Toolkit.WebApp.CustomConfigurationSection.ExtensionMethods
+{
+    public static class AppRoleExtensions
+    {
+        public static string GetDisplayName(this AppRole appRole)
+        {
+            switch (appRole)
+            {
+                case AppRole.ApplicationAdmin:
+                    return "Application Administrator";
+                case AppRole.WebsiteAdmin:
+                    return "Website Administrator";
+            }
+
+            return appRole.ToString("F");
+        }
+    }
+}
