8943715f807e01ce440003b6d7eece02948e6408
Leonard <lenny@erwinefamily.net>
2014-10-29 10:23:12 -0400

    Created AppRole.cs

---
+++ LTEWebAppToolKit/CustomConfigurationSection/AppRole.cs
@@ -0,0 +1,13 @@
+namespace Erwine.Leonard.T.Toolkit.WebApp.CustomConfigurationSection
+{
+    public enum AppRole : int
+    {
+        Deny = 0,
+        Reader = 1,
+        Customer = 2,
+        Auditor = 3,
+        Contributor = 4,
+        ApplicationAdmin = 5,
+        WebsiteAdmin = 6
+    }
+}
