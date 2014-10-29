ef1dc928927e07251cc26c4d91deef87fdd6a71d
Leonard <lenny@erwinefamily.net>
2014-10-29 16:31:55 -0400

    Create AssemblyInfo.cs

---
+++ SimplePaletteQuantizerModLTE/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("A Simple - Yet Quite Powerful - Palette Quantizer Library")]
+[assembly: AssemblyDescription("Color Palette Quantizer primarily for optimizing index-based images, such as Gif files.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Leonard T. Erwine")]
+[assembly: AssemblyProduct("SimplePaletteQuantizerModLTE")]
+[assembly: AssemblyCopyright("Leonard T. Erwine, October 2014, Code Project Open License (CPOL) 1.02")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("dcc146d0-4b23-4367-92c6-c35717233315")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
