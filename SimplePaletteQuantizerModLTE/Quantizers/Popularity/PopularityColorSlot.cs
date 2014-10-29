76ceb65e4432fc4804b024a485cfd09afd87cc17
Leonard <lenny@erwinefamily.net>
2014-10-29 18:26:00 -0400

    Update PopularityColorSlot.cs

--- SimplePaletteQuantizerModLTE/Quantizers/Popularity/PopularityColorSlot.cs
+++ SimplePaletteQuantizerModLTE/Quantizers/Popularity/PopularityColorSlot.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Drawing;
 
-namespace SimplePaletteQuantizer.Quantizers.Popularity
+namespace Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.Quantizers.Popularity
 {
     internal class PopularityColorSlot
     {
