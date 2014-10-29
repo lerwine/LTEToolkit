b6c3aa2010a3f61d6a3ef8c07517194a04fda816
Leonard <lenny@erwinefamily.net>
2014-10-29 18:04:48 -0400

    Update EuclideanDistanceColorCache.cs

--- SimplePaletteQuantizerModLTE/ColorCaches/EuclideanDistance/EuclideanDistanceColorCache.cs
+++ SimplePaletteQuantizerModLTE/ColorCaches/EuclideanDistance/EuclideanDistanceColorCache.cs
@@ -1,10 +1,10 @@
 using System;
 using System.Drawing;
 using System.Collections.Generic;
-using SimplePaletteQuantizer.Helpers;
-using SimplePaletteQuantizer.ColorCaches.Common;
+using Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.Helpers;
+using Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.ColorCaches.Common;
 
-namespace SimplePaletteQuantizer.ColorCaches.EuclideanDistance
+namespace Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.ColorCaches.EuclideanDistance
 {
     public class EuclideanDistanceColorCache : BaseColorCache
     {
