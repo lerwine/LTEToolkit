5c1f750f4a3ee2aa7f866e784f516fb23a6d086b
Leonard <lenny@erwinefamily.net>
2014-10-29 09:59:22 -0400

    Created HelperObject2.cs

---
+++ LTEToolkitUnitTest/SynchronizedLists/HelperObject2.cs
@@ -0,0 +1,12 @@
+namespace LTEToolkitUnitTest.SynchronizedLists
+{
+    public class HelperObject2
+    {
+        public int Id { get; private set; }
+
+        public HelperObject2(int id)
+        {
+            this.Id = id;
+        }
+    }
+}
