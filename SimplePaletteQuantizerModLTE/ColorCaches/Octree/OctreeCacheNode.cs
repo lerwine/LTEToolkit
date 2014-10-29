c7e2fba80e47cb9e43367de3386b33d20eb042c1
Leonard <lenny@erwinefamily.net>
2014-10-29 18:07:01 -0400

    Update OctreeCacheNode.cs

--- SimplePaletteQuantizerModLTE/ColorCaches/Octree/OctreeCacheNode.cs
+++ SimplePaletteQuantizerModLTE/ColorCaches/Octree/OctreeCacheNode.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 
-namespace SimplePaletteQuantizer.ColorCaches.Octree
+namespace Erwine.Leonard.T.Toolkit.SimplePaletteQuantizer.ColorCaches.Octree
 {
     public class OctreeCacheNode
     {
